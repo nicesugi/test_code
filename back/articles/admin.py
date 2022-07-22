4a75ef929119ad5aff7a53ca9519904fd5da59bd
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-22 23:34:17 +0900

    Feat : articles 생성 및 설정
    1. settings.py  앱 등록
    2. 프로젝트 urls 연결
    3. aricles model, serializer, url 생성
    4. article 어드민 등록

--- back/articles/admin.py
+++ back/articles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from articles.models import Article as ArticleModel
 
-# Register your models here.
+
+admin.site.register(ArticleModel)
---
+++ back/articles/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from articles.models import Article as ArticleModel
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleModel
+        fields= '__all__'
