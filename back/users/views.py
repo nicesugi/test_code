c2eb4192776e111a88fd9a8bceb12d417e4941b6
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-22 21:32:01 +0900

    Feat : 회원가입 기능 구현
    1. url 설정
    2. 회원가입 커스텀 serializer 생성
    3. 간단한 회원정보 조회, 회원가입 기능 구현

--- back/users/views.py
+++ back/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from users.serializers import UserSerializer
+from users.serializers import UserSignupSerializer
+
+
+class UserView(APIView):
+    # 사용자 정보 조회
+    def get(self, request):
+        return Response(UserSerializer(request.user).data, status=status.HTTP_200_OK)
+    
+    # 회원가입
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response({'message': '가입성공'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': '가입실패'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 회원정보 수정
+    def put(self, request):
+        return Response({})
+    
+    # 회원탈퇴
+    def delete(self, request):
+        return Response({})
