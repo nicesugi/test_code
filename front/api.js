3e18c845a45c29c40d110d4385cb66178c1bf881
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-22 23:14:58 +0900

    Feat : 로그아웃 기능 구현

--- front/api.js
+++ front/api.js
@@ -3,67 +3,75 @@ const frontend_base_url = 'http://127.0.0.1:5500'
 
 
 // 회원가입
-async function handleSignup(){
-
+async function handleSignup() {
     const signupData = {
-        username : document.getElementById('floatingInput').value,
-        password : document.getElementById('floatingPassword').value,
-        email : document.getElementById('floatingInputEmail').value,
-        fullname : document.getElementById('floatingInputFullname').value,
+        username: document.getElementById('floatingInput').value,
+        password: document.getElementById('floatingPassword').value,
+        email: document.getElementById('floatingInputEmail').value,
+        fullname: document.getElementById('floatingInputFullname').value,
     }
-    
-    const response = await fetch(`${backend_base_url}/users/`,{
+
+    const response = await fetch(`${backend_base_url}/users/`, {
         headers: {
             Accept: 'application/json',
             'Content-type': 'application/json'
         },
-        method:'POST',
-        body:JSON.stringify(signupData)
+        method: 'POST',
+        body: JSON.stringify(signupData)
     })
 
     response_json = await response.json()
 
-    if (response.status ==200){
+    if (response.status == 200) {
         window.location.replace(`${frontend_base_url}/front/login.html`);
-    }else{
+    } else {
         alert(response.status)
     }
 }
 
 
 // 로그인
-async function handleSignin(){
-
+async function handleSignin() {
     const signinData = {
-        username : document.getElementById('floatingInput').value,
-        password : document.getElementById('floatingPassword').value,
+        username: document.getElementById('floatingInput').value,
+        password: document.getElementById('floatingPassword').value,
     }
-    
-    const response = await fetch(`${backend_base_url}/users/api/token/`,{
+
+    const response = await fetch(`${backend_base_url}/users/api/token/`, {
         headers: {
             Accept: 'application/json',
             'Content-type': 'application/json'
         },
-        method:'POST',
-        body:JSON.stringify(signinData)
+        method: 'POST',
+        body: JSON.stringify(signinData)
     })
 
     response_json = await response.json()
 
     if (response.status == 200) {
-        localStorage.setItem("access", response_json.access);
-        localStorage.setItem("refresh", response_json.refresh);
+        localStorage.setItem('access', response_json.access);
+        localStorage.setItem('refresh', response_json.refresh);
 
-        const base64Url = response_json.access.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const base64Url = response_json.access.split('.')[1]
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
         const jsonPayload = decodeURIComponent(atob(base64).split('').map(
             function (c) {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            }).join(''));
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+            }).join(''))
 
-        localStorage.setItem("payload", jsonPayload);
+        localStorage.setItem('payload', jsonPayload)
         window.location.replace(`${frontend_base_url}/front/index.html`)
-    }else{
+    } else {
         alert(response.status)
     }
+}
+
+
+// 로그아웃
+async function handleLogout() {
+    localStorage.removeItem('access')
+    localStorage.removeItem('refresh')
+    localStorage.removeItem('payload')
+
+    location.reload()
 }
