508ae61813d9e3d273fcf32c3001cc9ded7ea226
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-22 23:34:38 +0900

    Feat : 게시글 전체조회, 게시글 작성 기능 구현

--- back/articles/views.py
+++ back/articles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from articles.models import Article as ArticleModel
+from articles.serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    # 게시글 전체 조회
+    def get(self, request):
+        articles = ArticleModel.objects.all()
+        serializer = ArticleSerializer(articles, many=True).data
+        
+        return Response(serializer, status=status.HTTP_200_OK)
+    
+    # 게시글 작성
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response({'message': '글 작성 완료!!'}, status=status.HTTP_200_OK)
+        
+        else:
+            return Response({'message': '글 작성 실패!'}, status=status.HTTP_400_BAD_REQUEST)
