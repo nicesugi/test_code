70ad9759664ff465ea1ed42228837382d358de2c
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-07-22 21:55:31 +0900

    Feat : 로그인 기능 구현(프론트) + token, payload 생성

--- front/api.js
+++ front/api.js
@@ -1,6 +1,8 @@
 const backend_base_url = 'http://127.0.0.1:8000'
 const frontend_base_url = 'http://127.0.0.1:5500'
 
+
+// 회원가입
 async function handleSignup(){
 
     const signupData = {
@@ -23,6 +25,44 @@ async function handleSignup(){
 
     if (response.status ==200){
         window.location.replace(`${frontend_base_url}/front/login.html`);
+    }else{
+        alert(response.status)
+    }
+}
+
+
+// 로그인
+async function handleSignin(){
+
+    const signinData = {
+        username : document.getElementById('floatingInput').value,
+        password : document.getElementById('floatingPassword').value,
+    }
+    
+    const response = await fetch(`${backend_base_url}/users/api/token/`,{
+        headers: {
+            Accept: 'application/json',
+            'Content-type': 'application/json'
+        },
+        method:'POST',
+        body:JSON.stringify(signinData)
+    })
+
+    response_json = await response.json()
+
+    if (response.status == 200) {
+        localStorage.setItem("access", response_json.access);
+        localStorage.setItem("refresh", response_json.refresh);
+
+        const base64Url = response_json.access.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(
+            function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+
+        localStorage.setItem("payload", jsonPayload);
+        window.location.replace(`${frontend_base_url}/front/index.html`)
     }else{
         alert(response.status)
     }
